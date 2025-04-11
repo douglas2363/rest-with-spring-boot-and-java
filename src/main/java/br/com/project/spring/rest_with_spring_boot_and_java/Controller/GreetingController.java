42276ac737101f4b14b3808fbfa29106f35736a8
Douglas <douglaslima2363@gmail.com>
2025-04-10 21:02:46 -0300

    Primeiro commit do projeto

---
+++ src/main/java/br/com/project/spring/rest_with_spring_boot_and_java/Controller/GreetingController.java
@@ -0,0 +1,21 @@
+package br.com.project.spring.rest_with_spring_boot_and_java.Controller;
+
+
+import br.com.project.spring.rest_with_spring_boot_and_java.Model.Greeting;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class GreetingController {
+
+    public static final  String template = "Hello, %s!";
+    public final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "word")  String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+}
---
+++ src/main/java/br/com/project/spring/rest_with_spring_boot_and_java/Model/Greeting.java
@@ -0,0 +1,3 @@
+package br.com.project.spring.rest_with_spring_boot_and_java.Model;
+
+public record Greeting(long id, String content) {}
